724d72314da1397b87893bb7a13b45f2c5b21f50
helgaRee <143488536+helgaRee@users.noreply.github.com>
2024-02-20 08:46:02 +0100

    first upload

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Ultimate Task Management Assistans";
+
+        return View();
+    }
+
+
+}
